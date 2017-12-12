d4d25b169f4f362448034ac6c6c9d5a0c939b744
faysoserious <32243939+faysoserious@users.noreply.github.com>
2017-12-12 10:52:08 +0100

    Update dHash.py

--- dHash.py
+++ dHash.py
@@ -1,36 +1,36 @@
 class DHash(object):
-    @staticmethod
+
     def calculate_hash(image):
         """
-        计算图片的dHash值
+        compute the dHash value for image
         :param image: PIL.Image
-        :return: dHash值,string类型
+        :return: dHash,(string)
         """
         difference = DHash.__difference(image)
-        # 转化为16进制(每个差值为一个bit,每8bit转为一个16进制)
+      
         decimal_value = 0
         hash_string = ""
         for index, value in enumerate(difference):
-            if value:  # value为0, 不用计算, 程序优化
+            if value:  # value为0, 
                 decimal_value += value * (2 ** (index % 8))
-            if index % 8 == 7:  # 每8位的结束
-                hash_string += str(hex(decimal_value)[2:].rjust(2, "0"))  # 不足2位以0填充。0xf=>0x0f
+            if index % 8 == 7: 
+                hash_string += str(hex(decimal_value)[2:].rjust(2, "0"))  # fill up with 0 0xf=>0x0f
                 decimal_value = 0
         return hash_string
 
-    @staticmethod
+
     def hamming_distance(first, second):
         """
-        计算两张图片的汉明距离(基于dHash算法)
-        :param first: Image或者dHash值(str)
-        :param second: Image或者dHash值(str)
-        :return: hamming distance. 值越大,说明两张图片差别越大,反之,则说明越相似
+        
+        :param first: Image or dHash (str)
+        :param second: Image dHash (str)
+        :return: hamming distance. 
         """
-        # A. dHash值计算汉明距离
+        # A. dHash calculate hamming distance
         if isinstance(first, str):
             return DHash.__hamming_distance_with_hash(first, second)
 
-        # B. image计算汉明距离
+        # B. calculate hamming_distance for images
         hamming_distance = 0
         image1_difference = DHash.__difference(first)
         image2_difference = DHash.__difference(second)
@@ -40,21 +40,16 @@ class DHash(object):
                 hamming_distance += 1
         return hamming_distance
 
-    @staticmethod
+
     def __difference(image):
-        """
-        *Private method*
-        计算image的像素差值
-        :param image: PIL.Image
-        :return: 差值数组。0、1组成
-        """
+
         resize_width = 9
         resize_height = 8
         # 1. resize to (9,8)
         smaller_image = image.resize((resize_width, resize_height))
-        # 2. 灰度化 Grayscale
+        # Grayscale
         grayscale_image = smaller_image.convert("L")
-        # 3. 比较相邻像素
+        # 
         pixels = list(grayscale_image.getdata())
         difference = []
         for row in range(resize_height):
@@ -64,15 +59,9 @@ class DHash(object):
                 difference.append(pixels[left_pixel_index] > pixels[left_pixel_index + 1])
         return difference
 
-    @staticmethod
+
     def __hamming_distance_with_hash(dhash1, dhash2):
-        """
-        *Private method*
-        根据dHash值计算hamming distance
-        :param dhash1: str
-        :param dhash2: str
-        :return: 汉明距离(int)
-        """
+
         difference = (int(dhash1, 16)) ^ (int(dhash2, 16))
         return bin(difference).count("1")
 
